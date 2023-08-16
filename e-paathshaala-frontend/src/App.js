470981bfbdd2a45f4e08aa0f5fe0b73107f1c558
Kumar Ankur <kumarankur2208@gmail.com>
2023-08-16 20:53:12 +0530

    Theme Change Check1

--- e-paathshaala-frontend/src/App.js
+++ e-paathshaala-frontend/src/App.js
@@ -13,26 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/Serv" element={<Serv />} />
-          <Route path="/Vid" element={<Vid />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Featured" element={<Featured />} />
-          <Route path="/Vehicles" element={<Vehicles />} />
-          <Route path="/AdminDashboard" element={<AdminDashboard />} />
-          <Route path="/UserDashboard" element={<UserDashboard />} />
-          <Route path="/AddNewCar" element={<AddNewCar />} />
-          <Route path="/AdminLogin" element={<AdminLogin />} /> */}
-          {/* <Route
-            path="/vehicles"
-            element={
-              <ProtectedRoute>
-                <Vehicles />
-              </ProtectedRoute>
-            }
-          /> */}
+        
 
           <Route path="*" element={<Notfound />} />
         </Routes>
