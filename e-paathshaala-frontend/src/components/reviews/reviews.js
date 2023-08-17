3b234d35366e66df2de82adfc97af285eea6fb87
Pranita Bondarde <pranitabondarde1993@gmail.com>
2023-08-18 00:08:11 +0530

    home+review changed

--- e-paathshaala-frontend/src/components/reviews/reviews.js
+++ e-paathshaala-frontend/src/components/reviews/reviews.js
@@ -20,9 +20,8 @@ export default function Reviews() {
                   <img src="./images/user2.png" alt="" />
                   <div className="content">
                     <p>
-                      From inspection to paperwork, I completed the entire
-                      process from the comfort of home. Lelo Gadi team was very
-                      professional and friendly.
+                      It was a nice experience and had a good and clear cut view
+                      on the topics. deep learning for lifetime.
                     </p>
                     <h3>Bhavit Rathod</h3>
                     <div className="stars">
@@ -49,9 +48,9 @@ export default function Reviews() {
                   />
                   <div className="content">
                     <p>
-                      We didn’t think selling our car could be so fast and easy.
-                      We received the best amount and it was same as the online
-                      estimate.
+                      e-Path-shala is an excellent website which has almost all
+                      the course one can think of at a very reasonable pricing
+                      from industry experts
                     </p>
                     <h3>Gaurav More</h3>
                     <div className="stars">
@@ -78,17 +77,20 @@ export default function Reviews() {
                   />
                   <div className="content">
                     <p>
-                      My friend recommended me Lelo Gadi and I am glad he did. The
-                      online quote was exactly matched after evaluation and
-                      payment was done within 30 minutes
+                      e-Path-shala offers practice exercises, instructional
+                      videos, and a personalized learning dashboard that empower
+                      students to study at their ownpace in and outside of the
+                      classroom.e-Path-shala focus on skill mastery to help
+                      learners establish strong foundations, so there is no
+                      limit to what they can learn next.
                     </p>
                     <h3>Omkar Bhojane</h3>
                     <div className="stars">
                       <i className="fas fa-star" />
                       <i className="fas fa-star" />
-                      <i className="fas fa-star-half-alt" />
-                      <i className="far fa-star" />
-                      <i className="far fa-star" />
+                      <i className="fas fa-star" />
+                      <i className="fas fa-star" />
+                      <i className="fas fa-star" />
                     </div>
                   </div>
                 </div>
@@ -107,9 +109,9 @@ export default function Reviews() {
                   />
                   <div className="content">
                     <p>
-                      The entire process was seamless and very quick. I checked
-                      the quote online and Lelo Gadi executive came for evaluation
-                      the same day. The entire process took less than 24 hours.
+                      e-Path-shala is designed in such a way that the student
+                      can select the subject to learn easily and Teachers can
+                      select the subject to teach easily.
                     </p>
                     <h3>Aditi Dravid</h3>
                     <div className="stars">
@@ -133,9 +135,9 @@ export default function Reviews() {
                   <img src="./images/user0.jpeg" alt="" />
                   <div className="content">
                     <p>
-                      Lelo Gadi becoming synonymous with trustworthy experience. I checked
-                      the online estimate and received the same amount after
-                      evaluation. The process is quick, simple and professional.
+                      This website provides information on educational school
+                      subjects for novice intermediates as well as expert,
+                      learners, teachers and parents.
                     </p>
                     <h3>Piyush Kulkarni</h3>
                     <div className="stars">
@@ -162,9 +164,8 @@ export default function Reviews() {
                   />
                   <div className="content">
                     <p>
-                      I love my car and with Lelo Gadi I got the confidence that
-                      the car will be in safe custody till it gets transferred
-                      to the new owner.
+                      Each new topic is hyperlinked to a page which provides
+                      detailed content on the subject.
                     </p>
                     <h3>Saiprasad Nimbalkar</h3>
                     <div className="stars">
